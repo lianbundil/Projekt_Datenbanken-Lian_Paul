d8d470ff7f02c9af991c35ddf48cdb337deb1203
lianbundil <168090060+lianbundil@users.noreply.github.com>
2024-05-16 13:18:47 +0200

    Add files via upload

---
+++ html projekt gitter.py
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+    <head>
+        <title>{{title}}</title>
+        <meta charset= "utf-8">
+        <meta description content="{{deswcription}}">
+        <link rel="shortcut icon" href="favicon.ico">
+        <link rel="stylesheet" type="text/css" href="/static/css/style.css"
+
+    </head>
+
+    <body> {
+  background-color: grey;    
+}
+
+    </body>
+
+
+</html>
